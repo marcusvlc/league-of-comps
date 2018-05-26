c1609a617cec1689a10904ed65b002427a2bceee
marcusvlc <marcus.costa@ccc.ufcg.edu.br>
2018-05-26 11:58:29 -0300

    Primeira aba do site

---
+++ src/main/java/com/composicao/comps/CompsApplication.java
@@ -0,0 +1,12 @@
+package com.composicao.comps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompsApplication.class, args);
+	}
+}
---
+++ src/main/java/com/composicao/comps/controllers/TelasController.java
@@ -0,0 +1,17 @@
+package com.composicao.comps.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@CrossOrigin
+public class TelasController {
+	
+	@RequestMapping("/")
+	public String principal() {
+		return "index";
+	}
+
+}
